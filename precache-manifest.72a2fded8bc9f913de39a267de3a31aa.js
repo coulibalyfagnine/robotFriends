1474c848443cd0358c2cacb816407dd49800caa7
coulibalyfagnine <coulibalyfagnine@gmail.com>
2020-03-28 14:50:19 +0100

    Updates

---
+++ precache-manifest.72a2fded8bc9f913de39a267de3a31aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8efa8fbcca28e162bffe61eb46e4e978",
+    "url": "/robotFriends/index.html"
+  },
+  {
+    "revision": "f47ab1ab89aa6ba9f1ad",
+    "url": "/robotFriends/static/css/2.07548e1c.chunk.css"
+  },
+  {
+    "revision": "b3ce305bdc7997dabe7a",
+    "url": "/robotFriends/static/css/main.548cccae.chunk.css"
+  },
+  {
+    "revision": "f47ab1ab89aa6ba9f1ad",
+    "url": "/robotFriends/static/js/2.55ac7e87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robotFriends/static/js/2.55ac7e87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3ce305bdc7997dabe7a",
+    "url": "/robotFriends/static/js/main.1cbf2631.chunk.js"
+  },
+  {
+    "revision": "b0d6eac8882885b303cb",
+    "url": "/robotFriends/static/js/runtime-main.ebf81262.js"
+  }
+]);
